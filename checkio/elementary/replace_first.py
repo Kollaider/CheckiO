96a3f389932ed917b0f5948dc97e5b2c33af7b9c
kolaider <kolja87@tut.by>
2021-02-05 22:09:44 +0300

    solved CheckiO task for Elementary named Remove Replace First

---
+++ checkio/elementary/replace_first.py
@@ -0,0 +1,28 @@
+"""Replace First.
+
+URL: https://py.checkio.org/en/mission/replace-first/
+DESCRIPTION:
+    In a given list the first element should become the last one.
+    An empty list or list with only one element should stay the same.
+INPUT/OUTPUT EXAMPLE:
+    list(replace_first([1, 2, 3, 4])) == [2, 3, 4, 1]
+    list(replace_first([1])) == [1]
+    list(replace_first([])) == []
+"""
+
+from typing import Iterable
+
+
+def replace_first(items: list) -> Iterable:
+    if len(items) > 1:
+        items.append(items.pop(0))
+    return items
+
+
+def main():
+    print('Example:')
+    print(replace_first([1, 2, 3, 4]))
+
+
+if __name__ == '__main__':
+    main()
