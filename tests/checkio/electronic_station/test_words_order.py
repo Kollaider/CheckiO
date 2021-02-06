0c57cf761f6ca7ec53e69999cfa1afbeddbd1d79
kolaider <kolja87@tut.by>
2021-02-06 11:14:16 +0300

    solved CheckiO task for Electronic Station named Words Order

---
+++ tests/checkio/electronic_station/test_words_order.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+
+from checkio.electronic_station.words_order import words_order
+
+
+class WordsOrderTestCase(TestCase):
+
+    def test__words_order__valid_result(self):
+        self.assertEqual(True, words_order('hi world im here',
+                                           ['world', 'here']))
+        self.assertEqual(False, words_order('hi world im here',
+                                            ['here', 'world']))
+        self.assertEqual(True, words_order('hi world im here',
+                                           ['world']))
+        self.assertEqual(False, words_order('hi world im here',
+                                            ['world', 'here', 'hi']))
+        self.assertEqual(True, words_order('hi world im here',
+                                           ['world', 'im', 'here']))
+        self.assertEqual(False, words_order('hi world im here',
+                                            ['world', 'hi', 'here']))
+        self.assertEqual(False, words_order('hi world im here',
+                                            ['world', 'world']))
+        self.assertEqual(False, words_order('hi world im here',
+                                            ['country', 'world']))
+        self.assertEqual(False, words_order('hi world im here',
+                                            ['wo', 'rld']))
+        self.assertEqual(False, words_order('',
+                                            ['world', 'here']))
