88ebea49ae6e049dd2603d71debbfd1ffa29d55a
kolaider <kolja87@tut.by>
2021-02-18 19:22:59 +0300

    fixed all recomendations, added __init__.py for unittest

--- checkio/elementary/all_upper_1.py
+++ checkio/elementary/all_upper_1.py
@@ -10,21 +10,15 @@ INPUT/OUTPUT EXAMPLE:
     is_all_upper('all lower') == False
     is_all_upper('mixed UPPER and lower') == False
     is_all_upper('') == True
-
 """
 
 
 def is_all_upper(text: str) -> bool:
-    for i in text:
-        if not i.isupper() and i.isalpha():
-            return False
-    else:
-        return True
+    return all(text.isupper() for ch in text if ch.isalpha())
 
 
 def main():
-    print('Example:')
-    print(is_all_upper('ALL UPPER'))
+    print(f"is_all_upper('ALL UPPER') == {is_all_upper('ALL UPPER')}")
 
 
 if __name__ == '__main__':
