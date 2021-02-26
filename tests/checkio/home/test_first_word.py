761ecd1f51f120f808c29a26f85575904c882303
Nikolay Strom <kolja87@tut.by>
2021-02-26 09:41:06 +0300

    added __init__.py

--- tests/checkio/home/test_first_word.py
+++ tests/checkio/home/test_first_word.py
@@ -6,9 +6,9 @@ from checkio.home.first_word import first_word
 class FirstWordTestCase(TestCase):
 
     def test__first_word__equal_result(self):
-        self.assertEqual('Hello', first_word("Hello world"))
+        self.assertEqual('Hello', first_word('Hello world'))
         self.assertEqual('a', first_word(' a word'))
-        self.assertEqual('don\'t', first_word('don\'t touch it'))
+        self.assertEqual("don't", first_word("don't touch it"))
         self.assertEqual('greetings', first_word('greetings, friends'))
         self.assertEqual('and', first_word('... and so on ...'))
         self.assertEqual('hi', first_word('hi'))
