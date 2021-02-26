0017300c43db800dda64d80af034a48e47a5d5a5
Nikolay Strom <kolja87@tut.by>
2021-02-26 11:45:43 +0300

    corrected recomendations from flake8

--- checkio/home/split_list.py
+++ checkio/home/split_list.py
@@ -2,8 +2,10 @@
 
 URL: https://py.checkio.org/en/mission/split-list/
 DESCRIPTION:
-    You have to split a given array into two arrays. If it has an odd amount of elements, then the first array should
-    have more elements. If it has no elements, then two empty arrays should be returned.
+    You have to split a given array into two arrays.
+    If it has an odd amount of elements, then the first array should
+    have more elements. If it has no elements, then two empty
+    arrays should be returned.
 INPUT/OUTPUT EXAMPLE:
     split_list([1, 2, 3, 4, 5, 6]) == [[1, 2, 3], [4, 5, 6]]
     split_list([1, 2, 3]) == [[1, 2], [3]]
