f364e42f58322b16ec783622773568493a182319
Nikolay Strom <kolja87@tut.by>
2021-02-26 10:50:10 +0300

    corrected recomendations from flake8

--- tests/checkio/home/test_popular_words.py
+++ tests/checkio/home/test_popular_words.py
@@ -6,7 +6,9 @@ from checkio.home.popular_words import popular_words
 class PopularWordsTestCase(TestCase):
 
     def test__popular_words__equal_result(self):
-        self.assertEqual({'i': 4, 'was': 3, 'three': 0, 'near': 0},
+        self.assertEqual(
+            {'i': 4, 'was': 3, 'three': 0, 'near': 0},
             popular_words(
-                'When I was One I had just begun When I was Two I was nearly new',
+                'When I was One I had just begun When '
+                'I was Two I was nearly new',
                 ['i', 'was', 'three', 'near']))
