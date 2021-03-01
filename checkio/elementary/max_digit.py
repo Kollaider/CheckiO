dfdccdc4edc700719a4910b926935818f10319ff
kolaider <kolja87@tut.by>
2021-03-01 21:07:45 +0300

    Solved Checkio task from Elementary named Max Digits

---
+++ checkio/elementary/max_digit.py
@@ -0,0 +1,25 @@
+"""Max Digits.
+
+URL: https://py.checkio.org/en/mission/max-digit/
+DESCRIPTION:
+    You have a number and you need to determine which digit
+    in this number is the biggest.
+INPUT/OUTPUT EXAMPLE:
+    max_digit(0) == 0
+    max_digit(52) == 5
+    max_digit(634) == 6
+    max_digit(1) == 1
+    max_digit(10000) == 1
+"""
+
+
+def max_digit(number: int) -> int:
+    return int(max(str(number)))
+
+
+def main():
+    print(f'max_digit(0) == {max_digit(0)}')
+
+
+if __name__ == '__main__':
+    main()
